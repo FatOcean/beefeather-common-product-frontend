1faa1a40dc10525c65a01748398a5e7e16490b22
张超 <zhangchao@linklogis.com>
2022-09-26 15:41:33 +0800

    驾驶证示例数据替换

--- src/views/driving-license/example.js
+++ src/views/driving-license/example.js
@@ -1,72 +1,268 @@
 // import exampleOne from "../../assets/images/document-images/1/示例.png";
-import exampleTwo from "../../assets/images/document-images/income/示例二.jpg";
-import exampleOne from "../../assets/images/document-images/income/示例一.jpg";
+import exampleOne from "../../assets/images/document-images/driving-license/驾驶证2.jpg";
+import exampleTwo from "../../assets/images/document-images/driving-license/驾驶证1.jpg";
 // import exampleThree from "../../assets/images/document-images/3/示例.png";
 
 const documents = [
   {
     "name": "示例一.jpg",
-    "requestId": "001",
+    "requestId": "002",
     "pages": [
       {
         "img": exampleOne,
-        "pageName": "/示例一_1.jpg",
-        "originalHeight": 1963,
-        "originalWidth": 1375,
+        "pageName": "/示例一.jpg",
+        "originalHeight": 1210,
+        "originalWidth": 1680,
         "imgRotatingDeg": 0,
         "analysisResult": [
           {
-            "id": 0,
-            "key": "工作单位",
-            "value": "深圳市小蜜蜂科技有限公司",
-            "coordinatesList": [
-              {
-                "x": 900,
-                "y": 1412
-              },
-              {
-                "x": 1190,
-                "y": 1401
-              },
-              {
-                "x": 1192,
-                "y": 1438
-              },
-              {
-                "x": 902,
-                "y": 1448
-              }
-            ]
-          },
-          {
             "id": 1,
-            "key": "月收入",
-            "value": "10000",
+            "key": "首页证号",
+            "value": "150000000000000000",
             "coordinatesList": [
-              {
-                "x": 394,
-                "y": 757
-              },
-              {
-                "x": 489,
-                "y": 759
-              },
-              {
-                "x": 487,
-                "y": 804
-              },
-              {
-                "x": 392,
-                "y": 801
-              }
+              [
+                {
+                  "x": 714,
+                  "y": 319
+                },
+                {
+                  "x": 1239,
+                  "y": 316
+                },
+                {
+                  "x": 1240,
+                  "y": 378
+                },
+                {
+                  "x": 715,
+                  "y": 381
+                }
+              ]
             ]
           },
           {
             "id": 2,
-            "key": "年收入",
-            "value": "",
+            "key": "首页姓名",
+            "value": "桃桃桃",
             "coordinatesList": [
-
+              [
+                {
+                  "x": 330,
+                  "y": 406
+                },
+                {
+                  "x": 668,
+                  "y": 406
+                },
+                {
+                  "x": 668,
+                  "y": 474
+                },
+                {
+                  "x": 330,
+                  "y": 474
+                }
+              ]
+            ]
+          },
+          {
+            "id": 3,
+            "key": "性别",
+            "value": "女",
+            "coordinatesList": [
+              [
+                {
+                  "x": 902,
+                  "y": 405
+                },
+                {
+                  "x": 963,
+                  "y": 405
+                },
+                {
+                  "x": 963,
+                  "y": 468
+                },
+                {
+                  "x": 902,
+                  "y": 468
+                }
+              ]
+            ]
+          },
+          {
+            "id": 4,
+            "key": "国籍",
+            "value": "中国",
+            "coordinatesList": [
+              [
+                {
+                  "x": 1172,
+                  "y": 400
+                },
+                {
+                  "x": 1294,
+                  "y": 400
+                },
+                {
+                  "x": 1294,
+                  "y": 472
+                },
+                {
+                  "x": 1172,
+                  "y": 472
+                }
+              ]
+            ]
+          },
+          {
+            "id": 5,
+            "key": "住址",
+            "value": "内蒙古自治区赤峰市",
+            "coordinatesList": [
+              [
+                {
+                  "x": 370,
+                  "y": 495
+                },
+                {
+                  "x": 885,
+                  "y": 495
+                },
+                {
+                  "x": 885,
+                  "y": 558
+                },
+                {
+                  "x": 370,
+                  "y": 558
+                }
+              ]
+            ]
+          },
+          {
+            "id": 6,
+            "key": "出生日期",
+            "value": "1999-09-09",
+            "coordinatesList": [
+              [
+                {
+                  "x": 715,
+                  "y": 666
+                },
+                {
+                  "x": 998,
+                  "y": 663
+                },
+                {
+                  "x": 999,
+                  "y": 724
+                },
+                {
+                  "x": 716,
+                  "y": 727
+                }
+              ]
+            ]
+          },
+          {
+            "id": 7,
+            "key": "初次领证日期",
+            "value": "2015-06-18",
+            "coordinatesList": [
+              [
+                {
+                  "x": 792,
+                  "y": 757
+                },
+                {
+                  "x": 1085,
+                  "y": 757
+                },
+                {
+                  "x": 1085,
+                  "y": 818
+                },
+                {
+                  "x": 792,
+                  "y": 818
+                }
+              ]
+            ]
+          },
+          {
+            "id": 8,
+            "key": "准驾车型",
+            "value": "C1",
+            "coordinatesList": [
+              [
+                {
+                  "x": 834,
+                  "y": 858
+                },
+                {
+                  "x": 978,
+                  "y": 857
+                },
+                {
+                  "x": 979,
+                  "y": 914
+                },
+                {
+                  "x": 835,
+                  "y": 916
+                }
+              ]
+            ]
+          },
+          {
+            "id": 9,
+            "key": "有效期限",
+            "value": "2015-06-18至2021-66-18",
+            "coordinatesList": [
+              [
+                {
+                  "x": 448,
+                  "y": 949
+                },
+                {
+                  "x": 1141,
+                  "y": 945
+                },
+                {
+                  "x": 1142,
+                  "y": 1006
+                },
+                {
+                  "x": 449,
+                  "y": 1009
+                }
+              ]
+            ]
+          },
+          {
+            "id": 10,
+            "key": "发证单位",
+            "value": "河北省秦皇\n岛市公安局\n交通警察支队",
+            "coordinatesList": [
+              [
+                {
+                  "x": 230,
+                  "y": 669
+                },
+                {
+                  "x": 516,
+                  "y": 663
+                },
+                {
+                  "x": 517,
+                  "y": 929
+                },
+                {
+                  "x": 231,
+                  "y": 930
+                }
+              ]
             ]
           }
         ]
@@ -75,72 +271,94 @@ const documents = [
   },
   {
     "name": "示例二.jpg",
-    "requestId": "002",
+    "requestId": "001",
     "pages": [
       {
         "img": exampleTwo,
-        "pageName": "/示例二(1)_1.jpg",
-        "originalHeight": 2419,
-        "originalWidth": 1814,
+        "pageName": "/示例二.jpg",
+        "originalHeight": 1080,
+        "originalWidth": 1440,
         "imgRotatingDeg": 0,
         "analysisResult": [
           {
-            "id": 0,
-            "key": "工作单位",
-            "value": "深圳市小蜜峰科技有限公司",
-            "coordinatesList": [
-              {
-                "x": 1108,
-                "y": 1356
-              },
-              {
-                "x": 1461,
-                "y": 1356
-              },
-              {
-                "x": 1461,
-                "y": 1701
-              },
-              {
-                "x": 1108,
-                "y": 1701
-              }
-            ]
-          },
-          {
             "id": 1,
-            "key": "月收入",
-            "value": "",
+            "key": "副页证号",
+            "value": "440000000000000000",
             "coordinatesList": [
-
+              [
+                {
+                  "x": 665,
+                  "y": 318
+                },
+                {
+                  "x": 1056,
+                  "y": 306
+                },
+                {
+                  "x": 1057,
+                  "y": 353
+                },
+                {
+                  "x": 666,
+                  "y": 366
+                }
+              ]
             ]
           },
           {
             "id": 2,
-            "key": "年收入",
-            "value": "250000",
+            "key": "副页姓名",
+            "value": "张张张",
             "coordinatesList": [
-              {
-                "x": 1181,
-                "y": 883
-              },
-              {
-                "x": 1317,
-                "y": 883
-              },
-              {
-                "x": 1317,
-                "y": 932
-              },
-              {
-                "x": 1181,
-                "y": 932
-              }
+              [
+                {
+                  "x": 412,
+                  "y": 377
+                },
+                {
+                  "x": 666,
+                  "y": 371
+                },
+                {
+                  "x": 667,
+                  "y": 423
+                },
+                {
+                  "x": 413,
+                  "y": 429
+                }
+              ]
+            ]
+          },
+          {
+            "id": 3,
+            "key": "档案编号",
+            "value": "440000000000",
+            "coordinatesList": [
+              [
+                {
+                  "x": 897,
+                  "y": 365
+                },
+                {
+                  "x": 1156,
+                  "y": 357
+                },
+                {
+                  "x": 1157,
+                  "y": 405
+                },
+                {
+                  "x": 898,
+                  "y": 414
+                }
+              ]
             ]
           }
         ]
       }
     ]
-  }
+  },
+
 ]
 export default documents
