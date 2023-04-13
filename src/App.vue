628981ff4f8b305011f82fc3bd53d129abb35e6a
zhonglingyun <zhonglingyun@linklogis.com>
2023-04-13 14:52:45 +0800

    fix：test

--- src/App.vue
+++ src/App.vue
@@ -11,19 +11,29 @@ export default {
   computed: {
     ...mapState(["pageMenuPerm"]),
   },
-  methods: {
-    ...mapMutations(["changePageMenuPerm"]),
+  watch: {
+    $route(val) {
+      this.messageIframeProduct();
+    },
   },
   mounted() {
     // 接收iframe的数据
-    window.addEventListener("message", (e) => {
+    window.addEventListener("message", this.messageIframeProduct);
+  },
+  methods: {
+    ...mapMutations(["changePageMenuPerm"]),
+    messageIframeProduct(e) {
       if (e && e.data && e.data.pageMenuPerm) {
+        console.log("zhix");
         const pageMenuPerm = e.data.pageMenuPerm;
         this.$nextTick(() => {
           this.changePageMenuPerm(pageMenuPerm);
         });
       }
-    });
+    },
+  },
+  beforeDestroy() {
+    window.removeEventListener("message", this.messageIframeProduct);
   },
 };
 </script>
