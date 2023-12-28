cbe1fe58463eacb0bbed6b0265cd3c7be460097c
张超 <zhangchao@linklogis.com>
2023-12-28 17:49:48 +0800

    fix(S23-25694):一些跳转优化逻辑及优化

--- src/components/table-wrapper/table-wrapper.vue
+++ src/components/table-wrapper/table-wrapper.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="table-wrapper" @scroll="scroll">
     <slot></slot>
-    <div
+    <!-- <div
       v-show="disY > 8"
       ref="virtual"
       class="virtual"
@@ -9,11 +9,11 @@
       :style="{ left: `${scrollLeft}px`, bottom: `${disY - 7}px` }"
     >
       <div :style="{ width: `${scrollWidth}px` }"></div>
-    </div>
+    </div> -->
   </div>
 </template>
 <script>
-import ResizeObserver from 'resize-observer-polyfill'
+import ResizeObserver from "resize-observer-polyfill";
 
 export default {
   data() {
@@ -21,64 +21,62 @@ export default {
       clientWidth: null,
       scrollWidth: null,
       scrollLeft: null,
-      disY: null
-    }
+      disY: null,
+    };
   },
   mounted() {
-    const el = this.$el
+    const el = this.$el;
     this.resizeObserver = new ResizeObserver((_) => {
       this.proxy((_) => {
-        this.clientWidth = this.$el.clientWidth
-        this.scrollWidth = this.$el.scrollWidth
-        this.updateVirtual()
-      })
-    })
+        this.clientWidth = this.$el.clientWidth;
+        this.scrollWidth = this.$el.scrollWidth;
+        this.updateVirtual();
+      });
+    });
     // console.log("listen")
-    this.$events.listen('ocr-text-scroll', this.updateVirtual)
-    this.resizeObserver.observe(el)
+    this.$events.listen("ocr-text-scroll", this.updateVirtual);
+    this.resizeObserver.observe(el);
   },
   beforeDestroy() {
-    this.$events.remove('ocr-text-scroll')
-    this.resizeObserver.disconnect()
+    this.$events.remove("ocr-text-scroll");
+    this.resizeObserver.disconnect();
   },
   methods: {
     scroll(e) {
-      this.scrollLeft = e.target.scrollLeft
-      this.$el.scrollLeft = this.scrollLeft
-      this.$refs.virtual.scrollLeft = this.scrollLeft
-      this.$emit('scroll')
+      this.scrollLeft = e.target.scrollLeft;
+      this.$el.scrollLeft = this.scrollLeft;
+      // this.$refs.virtual.scrollLeft = this.scrollLeft;
+      this.$emit("scroll");
     },
     updateVirtual() {
-      // console.log("scroll")
+      this.scrollLeft = this.$el.scrollLeft;
+      // this.$refs.virtual.scrollLeft = this.scrollLeft;
 
-      this.scrollLeft = this.$el.scrollLeft
-      this.$refs.virtual.scrollLeft = this.scrollLeft
-
-      const elRect = this.$el.getBoundingClientRect()
-      const parentRect = this.$el.parentNode.getBoundingClientRect()
-      const elBottom = elRect.bottom
-      const parentBottom = parentRect.bottom
-      const disY = elBottom - parentBottom
+      const elRect = this.$el.getBoundingClientRect();
+      const parentRect = this.$el.parentNode.getBoundingClientRect();
+      const elBottom = elRect.bottom;
+      const parentBottom = parentRect.bottom;
+      const disY = elBottom - parentBottom;
 
       // console.log(elBottom, parentBottom);
 
-      this.disY = disY
+      this.disY = disY;
     },
     proxy(fun, args) {
-      if (this.proxying) return
-      this.proxying = true
+      if (this.proxying) return;
+      this.proxying = true;
       window.requestAnimationFrame((_) => {
-        fun.call(this, args)
-        this.proxying = false
-      })
-    }
-  }
-}
+        fun.call(this, args);
+        this.proxying = false;
+      });
+    },
+  },
+};
 </script>
 <style lang="stylus">
 .table-wrapper {
   width: calc(100% - 16px);
-  overflow: auto;
+  // overflow: auto;
   position: relative;
 
   .virtual {
--- src/views/AgeneralOcr/index.vue
+++ src/views/AgeneralOcr/index.vue
@@ -3,256 +3,267 @@
     <lls-page-header @back="goBack" content="通用OCR工具" bottom-line>
     </lls-page-header>
     <div class="document-box">
-    <leftselect ></leftselect>
-    <ocrlayout
-      v-if="productName === 'document_ocr'"
-      :data="documents"
-      v-model="page"
-      :activeTabIndex="activeTabIndex"
-      ref="documents"
-      :pageMenuPerm="pageMenuPerm"
-      @resetId="() => (activeTextId = null)"
-      :coordinateData="page.content"
-      locatable
-      showAllCoordinate
-    >
-      <ocr-el
-        v-for="(i, index) in page.content"
-        :key="index"
-        :id="i.id"
-        :value="i"
-        :class="[`rect${i.id}`]"
-        >{{ i.value }}</ocr-el
+      <leftselect></leftselect>
+      <ocrlayout
+        v-if="productName === 'document_ocr'"
+        :data="documents"
+        v-model="page"
+        :activeTabIndex="activeTabIndex"
+        ref="documents"
+        :pageMenuPerm="pageMenuPerm"
+        @resetId="() => (activeTextId = null)"
+        :coordinateData="page.content"
+        locatable
+        showAllCoordinate
       >
-    </ocrlayout>
-    <ocrlayout
-      v-if="productName === 'form_ocr'"
-      v-model="page"
-      ref="documents"
-      :data="documents"
-      locatable
-      @on-resize="proxy(calculateXy)"
-      @on-scroll="proxy(calculateXy)"
-    >
-      <template v-for="(content, index) in page.content">
-        <!-- 表格 -->
-        <template v-if="content.table">
-          <table-wrapper
-            ref="tableWrapper"
-            :key="`rable${index}${page.pageNo}${page.requestId}`"
-            @scroll="proxy(calculateXy)"
-          >
-            <table cellspacing="0" cellpadding="0">
-              <tr
-                v-for="(columns, excelIndex) in content.table"
-                :key="`table${index}excel${excelIndex}`"
-              >
-                <ocr-el
-                  v-for="(column, columnIndex) in columns"
-                  :key="columnIndex"
-                  :value="column"
-                  :colspan="column.endCol - column.startCol + 1"
-                  :rowspan="column.endRow - column.startRow + 1"
-                  :style="{
-                    height: `${column.height * page.scale}px`,
-                  }"
-                  tag="td"
-                  v-html="resolveCol(column.value)"
-                >
-                </ocr-el>
-              </tr>
-            </table>
-          </table-wrapper>
-        </template>
-        <!-- 文本 -->
-        <template v-if="content.text">
-          <ocr-el
-            v-for="(text, textIndex) in content.text"
-            :key="`content${index}text${textIndex}`"
-            :value="text"
-            tag="div"
-            >{{ text.value }}</ocr-el
-          >
-        </template>
-      </template>
-    </ocrlayout>
-    <SealRecognition
-      v-if="productName === 'seal_recognition'"
-      @resetId="
-        () => {
-          activeTextId = null;
-          activeId = null;
-        }
-      "
-      :data="documents"
-      v-model="page"
-      ref="documents"
-      :activeTabIndex="activeTabIndex"
-      :productObj="productObj"
-    >
-      <div class="result-list">
-        <div
-          class="list-item"
-          v-for="item in page.content"
-          @click="(e) => listClick(e, item)"
-          :class="{ active: activeId === item.id }"
-          :key="item.id"
+        <ocr-el
+          v-for="(i, index) in page.content"
+          :key="index"
+          :id="i.id"
+          :value="i"
+          :class="[`rect${i.id}`]"
+          >{{ i.value }}</ocr-el
         >
-          <div class="head">{{ item.sealType }}</div>
+      </ocrlayout>
+      <ocrlayout
+        v-if="productName === 'form_ocr'"
+        v-model="page"
+        ref="documents"
+        :data="documents"
+        locatable
+        @on-resize="proxy(calculateXy)"
+        @on-scroll="proxy(calculateXy)"
+      >
+        <template v-for="(content, index) in page.content">
+          <!-- 表格 -->
+          <template v-if="content.table">
+            <table-wrapper
+              ref="tableWrapper"
+              :key="`rable${index}${page.pageNo}${page.requestId}`"
+              @scroll="proxy(calculateXy)"
+            >
+              <table cellspacing="0" cellpadding="0">
+                <tr
+                  v-for="(columns, excelIndex) in content.table"
+                  :key="`table${index}excel${excelIndex}`"
+                >
+                  <ocr-el
+                    v-for="(column, columnIndex) in columns"
+                    :key="columnIndex"
+                    :value="column"
+                    :colspan="column.endCol - column.startCol + 1"
+                    :rowspan="column.endRow - column.startRow + 1"
+                    :style="{
+                      height: `${column.height * page.scale}px`,
+                    }"
+                    tag="td"
+                    v-html="resolveCol(column.value)"
+                  >
+                  </ocr-el>
+                </tr>
+              </table>
+            </table-wrapper>
+          </template>
+          <!-- 文本 -->
+          <template v-if="content.text">
+            <ocr-el
+              v-for="(text, textIndex) in content.text"
+              :key="`content${index}text${textIndex}`"
+              :value="text"
+              tag="div"
+              >{{ text.value }}</ocr-el
+            >
+          </template>
+        </template>
+      </ocrlayout>
+      <SealRecognition
+        v-if="productName === 'seal_recognition'"
+        @resetId="
+          () => {
+            activeTextId = null;
+            activeId = null;
+          }
+        "
+        :data="documents"
+        v-model="page"
+        ref="documents"
+        :activeTabIndex="activeTabIndex"
+        :productObj="productObj"
+      >
+        <div class="result-list">
           <div
-            class="content"
-            v-for="i in item.texts"
-            :class="{ contentActive: activeTextId === i.id && activeId === item.id}"
-            @click.stop="(e) => clickHandler(e, i, item)"
-            :key="i.key"
+            class="list-item"
+            v-for="item in page.content"
+            @click="(e) => listClick(e, item)"
+            :class="{ active: activeId === item.id }"
+            :key="item.id"
           >
-            {{ i.text }}
+            <div class="head">{{ item.sealType }}</div>
+            <div
+              class="content"
+              v-for="i in item.texts"
+              :class="{
+                contentActive: activeTextId === i.id && activeId === item.id,
+              }"
+              @click.stop="(e) => clickHandler(e, i, item)"
+              :key="i.key"
+            >
+              {{ i.text }}
+            </div>
           </div>
         </div>
-      </div>
-    </SealRecognition>
-    <Sealdetection v-if="productName ==='seal_detection'" :data="documents"></Sealdetection>
-    <Sealremoval v-if="productName ==='seal_removal'"></Sealremoval>
+      </SealRecognition>
+      <Sealdetection
+        v-if="productName === 'seal_detection'"
+        :data="documents"
+      ></Sealdetection>
+      <Sealremoval v-if="productName === 'seal_removal'"></Sealremoval>
     </div>
   </div>
 </template>
 <script>
-import { staticData } from './staticData'
-import ocrlayout from './ocr-layout'
-import { mapState } from 'vuex'
-import leftselect from './components/leftselect.vue'
+import { staticData } from "./staticData";
+import ocrlayout from "./ocr-layout";
+import { mapState } from "vuex";
+import leftselect from "./components/leftselect.vue";
 export default {
   components: {
     ocrlayout,
     leftselect,
-    SealRecognition: (resolve) => require(['./ocr-layout/seal_recognition.vue'], resolve), // 印章识别
-    Sealdetection: (resolve) => require(['./seal_detection'], resolve), // 印章检测
-    Sealremoval: (resolve) => require(['./seal_removal'], resolve) // 印章去除
+    SealRecognition: (resolve) =>
+      require(["./ocr-layout/seal_recognition.vue"], resolve), // 印章识别
+    Sealdetection: (resolve) => require(["./seal_detection"], resolve), // 印章检测
+    Sealremoval: (resolve) => require(["./seal_removal"], resolve), // 印章去除
   },
   data() {
     return {
       // page: {}, // 当前页面数据信息
       percent: 0, // 进度条
-      activeName: '',
+      activeName: "",
       activeDocumentIndex: 0,
       checked: false,
       staticData,
       activeTabIndex: 0,
       tabsArray: [],
       activeTextId: 0,
-      productName: 'document_ocr',
+      productName: "document_ocr",
       activePageIndex: 0,
-      activeId: '',
+      activeId: "",
       productObj: {
-        name: '文档OCR',
-        staticName: 'document_ocr'
-      }
-    }
+        name: "文档OCR",
+        staticName: "document_ocr",
+      },
+    };
   },
 
   watch: {
     checked(val) {
-      this.$refs.documents.pathValue = null
-      this.filterEmpty(val)
-    }
+      this.$refs.documents.pathValue = null;
+      this.filterEmpty(val);
+    },
   },
   created() {
-    console.log(this.staticData)
+    console.log(this.staticData);
   },
   computed: {
     documents() {
-      const document = JSON.parse(JSON.stringify(staticData[this.productName]))
-      return document
+      const document = JSON.parse(JSON.stringify(staticData[this.productName]));
+      return document;
     },
     page() {
-      return this.documents[this.activePageIndex]
+      return this.documents[this.activePageIndex];
     },
     pageDetail() {
-      return this.page.content[this.activeTabIndex].identityList
+      return this.page.content[this.activeTabIndex].identityList;
     },
-    ...mapState(['pageMenuPerm']),
+    ...mapState(["pageMenuPerm"]),
     originLocation() {
-      return process.env.NODE_ENV === 'development'
-        ? 'https://beefeather-ng-front.lianyirong.com.cn//file-handle-web/file/image'
-        : `${window.location.origin}/file-handle-web/file/image`
-    }
+      return process.env.NODE_ENV === "development"
+        ? "https://beefeather-ng-front.lianyirong.com.cn//file-handle-web/file/image"
+        : `${window.location.origin}/file-handle-web/file/image`;
+    },
   },
   mounted() {},
   methods: {
     listClick(e, i) {
-      if (this.activeId === i.id) return
-      this.activeTextId = null
-      this.activeId = i.id
-      console.log(i)
+      if (this.activeId === i.id) return;
+      this.activeTextId = null;
+      this.activeId = i.id;
+      console.log(i);
       this.$refs.documents.handleClickRectangle({
-        item: i
-      })
+        item: i,
+      });
     },
     clickHandler(e, i, parent) {
-      const el = e.target
-      this.activeId = parent.id
-      e = e || window.event
+      const el = e.target;
+      this.activeId = parent.id;
+      e = e || window.event;
       this.$refs.documents.handleClickText({
         el,
         id: i.id,
-        item: i
-      })
-      this.activeTextId = this.$refs.documents.activeTextId
+        item: i,
+      });
+      this.activeTextId = this.$refs.documents.activeTextId;
     },
     calculateXy() {
-      this.$events.trigger('ocr-text-scroll')
-      this.$refs.documents.calculateXy()
+      this.$events.trigger("ocr-text-scroll");
+      this.$refs.documents.calculateXy();
     },
     setTableData(index) {
-      this.activePageIndex = this.$refs.documents.activePageIndex
+      this.activePageIndex = this.$refs.documents.activePageIndex;
     },
     setProductName(data) {
-      this.activeTextId = 0
-      this.productName = data.staticName
-      this.productObj = data
-      if (data.name === '文档OCR' || data.name === '表格OCR') this.$refs.documents.activeName = 'first'
+      this.activePageIndex = 0;
+      this.activeTextId = 0;
+      this.productName = data.staticName;
+      this.productObj = data;
+      if (data.name === "文档OCR" || data.name === "表格OCR")
+        this.$refs.documents.activeName = "first";
     },
     resetId() {},
     goBack() {},
     pushtest() {
-      const data = []
+      const data = [];
       data.map((item) => {
         item.imagePath = `${this.originLocation}?filename=${encodeURIComponent(
           item.imagePath
-        )}`
-      })
+        )}`;
+      });
     },
     uploadFileData(res) {
-      const data = res.data
+      const data = res.data;
       data.map((item) => {
         item.imagePath = `${this.originLocation}?filename=${encodeURIComponent(
           item.imagePath
-        )}`
-      })
-      this.staticData[this.productName] = data
+        )}`;
+      });
+      this.staticData[this.productName] = data;
     },
     resolveCol(value) {
-      return value.replace(/\n/gi, '<br/>')
+      return value.replace(/\n/gi, "<br/>");
     },
     proxy(fun, args) {
-      if (this.proxying) return
-      this.proxying = true
+      if (this.proxying) return;
+      this.proxying = true;
 
       window.requestAnimationFrame((_) => {
-        fun.call(this, args)
-        this.proxying = false
-      })
-    }
-  }
-}
+        fun.call(this, args);
+        this.proxying = false;
+      });
+    },
+  },
+};
 </script>
 <style lang="stylus" scoped>
 @import './index.styl';
-.document-box{
+
+.document-box {
   display: flex;
-  .ocr-layout{
+
+  .ocr-layout {
     flex: 1;
-    .ocr-inner{
+
+    .ocr-inner {
       width: 100%;
     }
   }
