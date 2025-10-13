931fdbd12e9f79490388f9316bd663812d621687
fatsea <ll041414@163.com>
2025-10-13 23:45:39 +0800

    feat：注释

--- src/views/tradeDocumentAnalysis/staticData/data.js
+++ src/views/tradeDocumentAnalysis/staticData/data.js
@@ -13,16 +13,16 @@ const productListAll = [
         staticName: 'order'
 
       },
-      {
-        name: '报关单',
-        staticName: 'customs_declaration'
+      // {
+      //   name: '报关单',
+      //   staticName: 'customs_declaration'
 
-      },
-      {
-        name: '航空单',
-        staticName: 'airway_bill'
+      // },
+      // {
+      //   name: '航空单',
+      //   staticName: 'airway_bill'
 
-      },
+      // },
       {
         name: '跨境合同',
         staticName: 'cross_border_contract'
@@ -35,14 +35,14 @@ const productListAll = [
     icon: '下载',
     isShowChildren: true,
     children: [
-      {
-        name: '商业发票',
-        staticName: 'commercial_invoice'
-      },
-      {
-        name: '银行承兑汇票',
-        staticName: 'bank_acceptance_bill'
-      },
+      // {
+      //   name: '商业发票',
+      //   staticName: 'commercial_invoice'
+      // },
+      // {
+      //   name: '银行承兑汇票',
+      //   staticName: 'bank_acceptance_bill'
+      // },
       {
         name: '流水',
         staticName: 'financial_statement'
@@ -50,11 +50,11 @@ const productListAll = [
       {
         name: '增值税发票',
         staticName: 'vat'
-      },
-      {
-        name: '回单',
-        staticName: 'receipt'
       }
+      // {
+      //   name: '回单',
+      //   staticName: 'receipt'
+      // }
     ]
   }
 ]
